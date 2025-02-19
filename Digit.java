75bd0cd6b28361f5720020a869505a401bb37a13
yugender936 <yugenderenugala936@gmail.com>
2025-02-19 19:15:19 +0530

    Add files via upload

---
+++ Digit.java
@@ -0,0 +1,15 @@
+class Digit
+{
+	public static void main(String[] args) 
+	{
+	    
+		char ch = 'a';
+		if((ch >= '0' && ch <= '9'))
+		{
+			System.out.println("Digit");
+		}
+		else{
+			System.out.println("Not a Digit");
+		}
+  }
+}
