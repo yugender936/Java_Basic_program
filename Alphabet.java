a1471706a79cb7a1003ff37690d12db4efe9ac5c
yugender936 <yugenderenugala936@gmail.com>
2025-02-19 13:10:25 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+class Alphabet
+{
+	public static void main(String[] args) 
+	{
+	    
+		char ch = '7';
+		if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+		{
+			System.out.println("Character");
+		}
+		else{
+			System.out.println("Not a character");
+		}
+  }
+}
