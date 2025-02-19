13ffec5d6523f8f50bfd43635b6d098d57e665a3
yugender936 <yugenderenugala936@gmail.com>
2025-02-19 13:11:44 +0530

    Add files via upload

---
+++ Evenorodd.java
@@ -0,0 +1,15 @@
+class Alphabet
+{
+	public static void main(String[] args) 
+	{
+	    int n = 4;
+		char ch = 'a';
+		if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+		{
+			System.out.println("Character");
+		}
+		else{
+			System.out.println("Not a character");
+		}
+  }
+}
