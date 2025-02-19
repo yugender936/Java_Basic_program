544276a7702dadf288b48cbfd60d23bb9ac170df
yugender936 <yugenderenugala936@gmail.com>
2025-02-19 19:05:10 +0530

    Add files via upload

---
+++ Posorneg.java
@@ -0,0 +1,13 @@
+class Evenorodd
+{
+	public static void main(String[] args) 
+	{
+	    int n = 4;
+		if(n%2 == 0){
+			System.out.println("Even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+  }
+}
