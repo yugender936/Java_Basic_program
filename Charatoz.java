1790a7eeadfdff58802a45de89021b1da606fe6c
yugender936 <yugenderenugala936@gmail.com>
2025-02-20 22:36:32 +0530

    Add files via upload

---
+++ Charatoz.java
@@ -0,0 +1,13 @@
+class Charatoz
+{
+	public static void main(String[] args) 
+	{
+	    
+		for(char i = 'A' ; i <= 'z' ; i++){
+			if((i >= 'A' && i <= 'Z') || (i >= 'a' && i <= 'z')){
+			System.out.print(i + " ");
+			}
+		}
+
+  }
+}
