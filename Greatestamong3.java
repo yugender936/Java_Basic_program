e4454aa885025a272057858613a1ad60f336a73f
yugender936 <yugenderenugala936@gmail.com>
2025-02-21 19:17:14 +0530

    Add files via upload

---
+++ Greatestamong3.java
@@ -0,0 +1,15 @@
+class Greatestamong3
+{
+	public static void main(String[] args) 
+	{
+	    
+		int a = 30;
+		int b = 140;
+		int c = 70;
+
+		int d = (a>b&&a>c)? a :(b>c)? b: c;
+		
+		System.out.println(d);
+
+  }
+}
