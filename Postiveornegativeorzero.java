55000eefb856d9a1b85df5792b619a78921c6d59
yugender936 <yugenderenugala936@gmail.com>
2025-02-21 19:16:30 +0530

    Add files via upload

---
+++ Postiveornegativeorzero.java
@@ -0,0 +1,15 @@
+class Postiveornegativeorzero
+{
+	public static void main(String[] args) 
+	{
+	    
+		int a = 0;
+
+		String b = (a > 0)?"Postive": (a<0)? "Negative" : "Zero";
+
+		System.out.println(b);
+		
+		
+
+  }
+}
