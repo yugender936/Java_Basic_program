11b543ac326c5e82e5e192e33f38f514545390cf
yugender936 <yugenderenugala936@gmail.com>
2025-02-21 19:18:19 +0530

    Add files via upload

---
+++ Factor.java
@@ -0,0 +1,13 @@
+class Factor
+{
+	public static void main(String[] args) 
+	{
+	    
+		int a = 3;
+		
+		boolean b = ((a%10 == 0) && (a%3 == 0))? true : false;
+
+		System.out.println(b);
+
+  }
+}
