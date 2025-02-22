333cd01561c5dc1519d24e7342f772efd485c755
yugender936 <yugenderenugala936@gmail.com>
2025-02-22 12:41:49 +0530

    Add files via upload

---
+++ SumofNumbers.java
@@ -0,0 +1,15 @@
+class SumofNumbers
+{
+	public static void main(String[] args) 
+	{
+	    
+		int sum = 0;
+		for(int i = 13 ; i <= 21 ; i++){
+			sum = sum + i;
+		}
+		System.out.println(sum);
+		
+		
+
+  }
+}
