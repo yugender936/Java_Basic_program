98a63add269f448658b12e219e7cdfa462cd5947
yugender936 <yugenderenugala936@gmail.com>
2025-02-22 12:42:51 +0530

    Add files via upload

---
+++ Givencharacter.java
@@ -0,0 +1,18 @@
+class Givencharacter
+{
+	public static void main(String[] args) 
+	{
+	    
+		char ch = 'A';
+		int n = ch;
+		if(n % 2 == 0){
+			System.out.println("Given character ascci value is even");
+		}
+		else{
+			System.out.println("Given character ascii value is odd");
+		}
+			
+		
+		
+  }
+}
