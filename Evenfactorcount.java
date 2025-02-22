1b25b1e2616bc9592022db12211564f80116b820
yugender936 <yugenderenugala936@gmail.com>
2025-02-22 12:41:30 +0530

    Add files via upload

---
+++ Evenfactorcount.java
@@ -0,0 +1,20 @@
+class Evenfactorcount
+{
+	public static void main(String[] args) 
+	{
+	    
+		int a = 10;
+        int count = 0;
+		for(int i = 1 ; i <= 10 ; i++){
+			if(10 % i == 0){
+				if(i % 2 ==0){
+				count++;
+				}
+			}
+		}
+		System.out.println(count);
+		
+		
+
+  }
+}
