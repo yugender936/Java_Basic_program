5c8ad06ba7c6783cbf11cb4c99cd4b4bc0c600b8
yugender936 <yugenderenugala936@gmail.com>
2025-02-22 12:47:43 +0530

    Add files via upload

---
+++ GivenNumberchar.java
@@ -0,0 +1,18 @@
+class GivenNumberchar
+{
+	public static void main(String[] args) 
+	{
+	    
+		int n = 75;
+		for(int i = 1 ; i <= n ; i++){
+			if(n % i == 0){
+				char ch = (char)i;
+				System.out.println(ch);
+			}
+		}
+			
+		
+		
+
+  }
+}
