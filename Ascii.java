3bbb1b7d67b220d1dcba2af82b36d983fe10364f
yugender936 <yugenderenugala936@gmail.com>
2025-02-22 12:42:11 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,20 @@
+class Ascii
+{
+	public static void main(String[] args) 
+	{
+	    
+		int n = 65;
+		for(char i = 'A' ; i <= 'Z' ; i++){
+			System.out.println(i + "--->" + n);
+			n++;
+		}
+		int m = 97;
+		for(char i = 'a' ; i <= 'z' ; i++){
+			System.out.println(i + "--->" + m);
+			m++;
+		}
+		
+		
+
+  }
+}
